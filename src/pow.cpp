71caeefa50202e6b396922405d8ecf35eab49968
smupyknight <smupyknight@outlook.com>
2018-04-13 00:34:15 +0800

    Fix printf

--- src/pow.cpp
+++ src/pow.cpp
@@ -126,7 +126,7 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     //printf("PoW Check Count: %d\n", nPoWCheckCount++);
-    printf("PoW nBits: nBits\n", nBits);
+    printf("PoW nBits: nBits%d\n", nBits);
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget) {
