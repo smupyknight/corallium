f21e41c14663e508c1a40afcb738d22c37b799b5
smupyknight <smupyknight@outlook.com>
2018-04-12 23:23:37 +0800

    Add PoW miner printf

--- src/pow.cpp
+++ src/pow.cpp
@@ -124,9 +124,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     // Check proof of work matches claimed amount
-    if (hash > bnTarget)
+    if (hash > bnTarget) {
+        printf("%s", hash.ToString().c_str());
         return false;
         //return error("CheckProofOfWork() : hash doesn't match nBits");
+    }
 
     return true;
 }
