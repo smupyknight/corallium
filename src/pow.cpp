9072ec7ac6ab4052a8b05c1db2fd622d9e0c0a86
smupyknight <smupyknight@outlook.com>
2018-04-13 00:36:00 +0800

    Display target hash

--- src/pow.cpp
+++ src/pow.cpp
@@ -126,7 +126,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
         return error("CheckProofOfWork() : nBits below minimum work");
 
     //printf("PoW Check Count: %d\n", nPoWCheckCount++);
-    printf("PoW nBits: nBits%d\n", nBits);
+    //printf("PoW nBits: nBits%d\n", nBits);
+    printf("Target Hash: %s\n", bnTarget.ToString().c_str());
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget) {
