9079358be6dc016b78366563f917a38b0e183ede
smupyknight <smupyknight@outlook.com>
2018-04-12 23:56:20 +0800

    Fix log

--- src/pow.cpp
+++ src/pow.cpp
@@ -125,7 +125,8 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
     if (fNegative || bnTarget == 0 || fOverflow || bnTarget > Params().ProofOfWorkLimit())
         return error("CheckProofOfWork() : nBits below minimum work");
 
-    printf("PoW Check Count: %d\n", nPoWCheckCount ++);
+    printf("PoW Check Count: %d\n", nPoWCheckCount++);
+    printf("PoW Check Count: %d\n", nPoWCheckCount++);
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget) {
