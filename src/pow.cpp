59b27af36d4ccdbf7dd8e6a38806794e4f7c08a0
smupyknight <smupyknight@outlook.com>
2018-04-12 23:48:13 +0800

    Add PoW check count monitor

--- src/pow.cpp
+++ src/pow.cpp
@@ -108,6 +108,8 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     return bnNew.GetCompact();
 }
 
+bool nPoWCheckCount = 0;
+
 bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 {
     bool fNegative;
@@ -125,11 +127,11 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits)
 
     // Check proof of work matches claimed amount
     if (hash > bnTarget) {
-        printf("%s\n", hash.ToString().c_str());
         return false;
         //return error("CheckProofOfWork() : hash doesn't match nBits");
     }
 
+    printf("PoW Check Count: %d\n", nPoWCheckCount ++);
     return true;
 }
 
